ef3756500c641c8627e5452a9494a6e92f75ea9b
brice <bricerabouin@hotmail.com>
2024-04-18 10:03:47 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import BackgroundGradientAnimation from "./components/ui/background-gradient-animation";
+
+function App() {
+  return (
+    <div className="App">
+      <BackgroundGradientAnimation />
+      {/* Autres composants et contenu */}
+    </div>
+  );
+}
+
+export default App;
